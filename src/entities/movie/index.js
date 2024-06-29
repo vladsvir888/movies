599f68951b64e3c0e3314d460191ae3900aaea8d
vsvirydovich <vladsvir888@gmail.com>
2024-06-29 23:30:37 +0300

    work on fsd

--- src/entities/movie/index.js
+++ src/entities/movie/index.js
@@ -1,3 +1,3 @@
-export { default as Card } from "./ui/card";
-export { default as Video } from "./ui/video";
+export { default as Card } from "./ui/card/Card.vue";
+export { default as Video } from "./ui/video/Video.vue";
 export * from "./model";
