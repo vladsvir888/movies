05f3e39b18b1a9f371bdeaeb81f3c9bf0f30ecc2
vsvirydovich <vladsvir888@gmail.com>
2024-09-21 16:16:30 +0300

    add change in auth middleware

--- src/entities/user/model/auth-middleware.ts
+++ src/entities/user/model/auth-middleware.ts
@@ -2,12 +2,8 @@ import { authSessionId } from "../config";
 
 export default defineNuxtPlugin(() => {
   addRouteMiddleware("auth", () => {
-    if (import.meta.client) {
-      return;
-    }
-
     if (useCookie(authSessionId).value) {
-      return navigateTo("/");
+      return abortNavigation();
     }
   });
 });
